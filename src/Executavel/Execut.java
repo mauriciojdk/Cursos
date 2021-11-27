ff6ce95196942431f125f9ea1faeb55cb15a4a2f
Mauricio Silva <mauriciojdk@gmail.com>
2021-11-27 19:52:40 -0300

    Adicionando Try catch

--- src/Executavel/Execut.java
+++ src/Executavel/Execut.java
@@ -11,12 +11,12 @@ import Classes.Disciplina;
 
 public class Execut {
     public static void main(String[] args) {
-
+		try {
 		List<Aluno> alunos = new ArrayList<Aluno>();
 
 		for (int qtd = 1; qtd <= 2; qtd++){
 
-			
+
 			Aluno aluno1 = new Aluno();
 			/*Student Data*/
 			aluno1.setNome(JOptionPane.showInputDialog("Qual nome do aluno "+ qtd + " ?"));;
@@ -29,28 +29,28 @@ public class Execut {
 			aluno1.setDataMatricula(JOptionPane.showInputDialog("Qual é a data de matricula do aluno? "));;
 			aluno1.setNomeEscola(JOptionPane.showInputDialog("Qual nome da escola? "));;
 			aluno1.setSerieMatriculado(JOptionPane.showInputDialog("Em qual serie aluno está matriculado? "));;
-			
+
 			/*Student Grades*/
 			for (int i = 1; i <= 4; i++){
 				String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+ i +" :");
 				String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina "+ i +" :");
-				
+
 				Disciplina disciplina = new Disciplina();
 				disciplina.setDisciplina(nomeDisciplina);
 				disciplina.setNota(Double.valueOf(notaDisciplina));
-				
+
 				aluno1.getDisciplinas().add(disciplina);
 			};
-			
+
 			int  resposta = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina? ");
-			
+
 			if (resposta == 0) {
-				
+
 				int continuarRemover = 0;
 				int posicao = 1;
-				
+
 				while (continuarRemover == 0) {
-					
+
 					String disciplina = (JOptionPane.showInputDialog("Qual disciplina sera removida? 1, 2, 3 ou 4: "));
 					aluno1.getDisciplinas().remove((Integer.parseInt(disciplina)) - posicao);
 					posicao ++;
@@ -58,12 +58,12 @@ public class Execut {
 				}
 				/*for (int test = 0; test <= 3 ; test++) {
 					System.out.println(aluno1.getDisciplinas().toString());   // - Show the list
-				}*/ 
+				}*/
 			}
 			alunos.add(aluno1);
 		}
 
-			
+
 		for (Aluno aluno : alunos) {
 			/* Show student text */
 			if (aluno.getNome().equalsIgnoreCase("alex")) {
@@ -75,7 +75,7 @@ public class Execut {
 			"Data de nascimento: " + aluno.getDataNascimento() + "\n" +
 			"RG: " + aluno.getRegistroGeral() + "\n" +
 			"CPF: " + aluno.getNumeroCpf() + "\n" +
-			"Mãe do aluno: " + aluno.getNomeMae() + "\n" + 
+			"Mãe do aluno: " + aluno.getNomeMae() + "\n" +
 			"Pai do aluno:" + aluno.getNomePai() + "\n" +
 			"Data de matricula: " + aluno.getDataMatricula() + "\n" +
 			"Nome da escola:  " + aluno.getNomeEscola() + "\n" +
@@ -83,7 +83,10 @@ public class Execut {
 			"Situação do aluno: " + aluno.getAlunoAprovado());
 			}
 		}
-			
 
-    }
+
+    }catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 }
